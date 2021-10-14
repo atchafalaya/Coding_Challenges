4a2f8d94c9cd733c4520c01d2b6f61e0900a2c1a
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-10-13 20:59:18 -0500

    Initial commit

---
+++ FindRange_Proctor.js
@@ -0,0 +1,14 @@
+const array = [28, 43, -12, 30, 4, 0, -36];
+
+const MinMax = () => {
+    let min = array [0], max = array [0];
+
+    for (let i =1; i < array.length; i++){
+        let value = array[i]
+        min = (value<min) ? value : min
+        max = (value>max) ? value : max
+    }
+    return [min, max];
+}
+const [LoopMin, LoopMax] = MinMax();
+console.log (`Minimum: ${LoopMin}, Maximum: ${LoopMax}`);
