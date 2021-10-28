ccf3bd02df5cdbbbf717f948e8d9aabb89d3e563
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-10-27 21:31:03 -0500

    27 October String Decoder

---
+++ String_Decoder.js
@@ -0,0 +1,22 @@
+const codeArray = ["2","b","n","a","0","p","1","m","p","2","o","s","l","0","e"]
+const decodeArray = codeArray.map(decodeFunction){
+
+    function decodeFunction(value, index, array) {
+        
+    }
+
+}
+
+
+
+
+// let n = 0;
+// while (n < String.length){
+//     n++
+// }
+// if (isNaN[n] = false){
+//     let n = n+n;
+// }else{
+// let decodeArray[n] = decodeArray.push[n];
+// }
+// console.log(decodeArray[n])
